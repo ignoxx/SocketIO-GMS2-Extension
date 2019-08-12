45f728d09562412a9750c7557209f6c71344e115
IGnoXX <ignoxx@yahoo.de>
2019-08-12 21:00:14 +0200

    We turn the packet into a string, if we received a JSON object
    Convert the received packet to string if its typeof 'object'.
    The server should be able send data as a string or as a JSON object
    
    Signed-off-by: IGnoXX <ignoxx@yahoo.de>

--- Source/gms2.api.js
+++ Source/gms2.api.js
@@ -7,46 +7,49 @@
 // Small wrapper of Socket.io for GM:S 2
 class SocketIO {
 
-    constructor(){
+    constructor() {
         this.socket;
         this.ip;
         this.port;
     }
 
-    connect(ip, port){
+    connect(ip, port) {
         this.ip = ip;
         this.port = port;
 
         this.socket = io.connect(`http://${this.ip}:${this.port}`);
 
-        this.socket.on('connect' ,() => {
+        this.socket.on('connect', () => {
             gml_Script_gmcallback_sio_on_connect();
-        }); 
+        });
 
-        this.socket.on('disconnect' ,() => {
+        this.socket.on('disconnect', () => {
             gml_Script_gmcallback_sio_on_disconnect();
-        }); 
+        });
     }
 
-    disconnect(){
+    disconnect() {
         this.socket.close();
     }
 
-    reconnect(){
+    reconnect() {
         this.socket.open();
     }
 
-    addEvent(name){
+    addEvent(name) {
         this.socket.on(name, (data) => {
+            if (typeof data === 'object')
+                data = JSON.stringify(data);
+
             window[`gml_Script_gmcallback_sio_on_${name.toLowerCase()}`](-1, -1, data);
         });
     }
 
-    send(name, data){
+    send(name, data) {
         this.socket.emit(name.toLowerCase(), data);
     }
 
-    getConnectionStatus(){
+    getConnectionStatus() {
         return this.socket.connected;
     }
 }
@@ -54,26 +57,26 @@ class SocketIO {
 // API for GM:S 2
 const socketio = new SocketIO();
 
-function sio_connect(ip, port){
+function sio_connect(ip, port) {
     socketio.connect(ip, port);
 }
 
-function sio_disconnect(){
+function sio_disconnect() {
     socketio.disconnect();
 }
 
-function sio_reconnect(){
+function sio_reconnect() {
     socketio.reconnect();
 }
 
-function sio_addEvent(name){
+function sio_addEvent(name) {
     socketio.addEvent(name);
 }
 
-function sio_emit(name, data){
+function sio_emit(name, data) {
     socketio.send(name, data);
 }
 
-function sio_get_connection_status(){
+function sio_get_connection_status() {
     return socketio.getConnectionStatus();
 }
