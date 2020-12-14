0e278e1d6eb8c47b02bbe051587aff9cc550464c
Ignas <ignas@DESKTOP-IVEIPJU.localdomain>
2020-12-14 22:33:58 +0100

    Update socket.io to v3.0.4

--- Source/gms2.api.js
+++ Source/gms2.api.js
@@ -1,15 +1,45 @@
 /*
     Gamemaker: Studio 1.x/2 Socket.io extension
-    Author:         Ignas Kavaliauskas (Inspired by Ivan Fonseca)
-    Source:         https://github.com/IgnasKavaliauskas/SocketIO-GMS2-Extension
-    Contributors:   https://github.com/IgnasKavaliauskas/SocketIO-GMS2-Extension/graphs/contributors
+    Source: https://github.com/IgnasKavaliauskas/SocketIO-GMS2-Extension
 */
+
+// API for GM:S 2
+const socketio = new SocketIO();
+
+function sio_connect() {
+    socketio.connect();
+}
+
+function sio_connect_by_url(url) {
+    socketio.connect_by_url(url);
+}
+
+function sio_disconnect() {
+    socketio.disconnect();
+}
+
+function sio_reconnect() {
+    socketio.reconnect();
+}
+
+function sio_addEvent(name) {
+    socketio.add_event(name);
+}
+
+function sio_emit(name, data) {
+    socketio.send(name, data);
+}
+
+function sio_get_connection_status() {
+    return socketio.get_connection_status();
+}
+
 
 // Small wrapper of Socket.io for GM:S 2
 class SocketIO {
 
     constructor() {
-        this.socket;
+        this.socket = null;
     }
 
     connect() {
@@ -56,35 +86,4 @@ class SocketIO {
     get_connection_status() {
         return this.socket.connected;
     }
-}
-
-// API for GM:S 2
-const socketio = new SocketIO();
-
-function sio_connect() {
-    socketio.connect();
-}
-
-function sio_connect_by_url(url) {
-    socketio.connect_by_url(url);
-}
-
-function sio_disconnect() {
-    socketio.disconnect();
-}
-
-function sio_reconnect() {
-    socketio.reconnect();
-}
-
-function sio_addEvent(name) {
-    socketio.add_event(name);
-}
-
-function sio_emit(name, data) {
-    socketio.send(name, data);
-}
-
-function sio_get_connection_status() {
-    return socketio.get_connection_status();
 }
